9b324c49e609cc1dd7ac98a80aaea997c6fa152b
Rics <danil.klementev.29@gmail.com>
2025-01-26 01:00:50 +0300

    add private route and sign in and sign out btn, also profile page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import {Footer} from "@/components/Footer";
+import { Providers } from "@/components/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,11 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Providers>
         <Header />
         <main className="container">
           {children}
         </main>
         <Footer />
+      </Providers>
       </body>
     </html>
   );
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,8 @@
-'use client'
+"use client";
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 
 type NavLink = {
   label: string;
@@ -12,18 +13,26 @@ type Props = {
   navLinks: NavLink[];
 }
 
-const Navigation = ({ navLinks } : Props) => {
+const Navigation = ({ navLinks }: Props) => {
   const pathName = usePathname();
+  const session = useSession();
 
-  return <>
-    {
-      navLinks.map((link) => {
-        const isActive = pathName === link.href;
 
-        return <Link href={link.href} key={link.label} className={isActive ? "active" : ""}>{link.label}</Link>
-      })
-    }
-  </>
-}
+  return (
+    <>
+      {
+        navLinks.map((link) => {
+          const isActive = pathName === link.href;
+
+          return <Link href={link.href} key={link.label} className={isActive ? "active" : ""}>{link.label}</Link>;
+        })
+      }
+      {session?.data && (
+        <Link href="/profile">Profile</Link>
+      )}
+      {session?.data ? <Link href="#" onClick={() => signOut({callbackUrl: "/"})}>Sign out</Link> : <Link href="/api/auth/signin">Sign in</Link>}
+    </>
+  );
+};
 
 export default Navigation;
