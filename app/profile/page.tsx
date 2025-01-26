e2fd7ed7316caf98fbb1f1dc3738e118d82d2b75
Rics <danil.klementev.29@gmail.com>
2025-01-27 00:36:42 +0300

    feat: add authorization

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { authConfig } from "@/configs/auth";
-import Image from "next/image";
 
 export default async function Profile(){
   const session = await getServerSession(authConfig );
